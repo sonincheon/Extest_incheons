3c2ed9863b2793258319d2d589cdfd22d5e0219b
incheons <sonincheon94@naver.com>
2024-03-12 16:23:07 +0900

    2024/03/12 손인천 알고리즘 연습

---
+++ src/알고리즘연습240312/ex38.java
@@ -0,0 +1,22 @@
+package 알고리즘연습240312;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ex38 {
+    public ArrayList<Integer> solution(int[] array, int[][] commands) {
+        ArrayList<Integer> answer =new ArrayList<>();
+        for(int[] a : commands){
+            answer.add(change(array,a));
+        }
+        return answer;
+    }
+    public int change (int[] array, int[] b){
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = b[0]-1;i<=b[1]-1;i++){
+            list.add(array[i]);
+        }
+        Collections.sort(list);
+      return list.get(b[2]-1);
+    }
+}
